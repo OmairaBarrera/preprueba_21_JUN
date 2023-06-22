694c13d6614029af35fb5ce81bab1596fb78a99a
OmairaBarrera <lomaira.barrera@gmail.com>
2023-06-22 08:39:09 -0500

    feat: :sparkles: connection to classes
    The connection was made with an autoload

--- scripts/areas/areas.php
+++ scripts/areas/areas.php
@@ -1,8 +1,9 @@
 <?php
     class areas extends connect{
         use getInstance;
-        function __construct(private $id, public $name_area){
+        function __construct(private $id=1, public $name_area=1){
             parent::__construct();
+            print_r("areas");
         }
     }
 ?>
--- scripts/db/connect.php
+++ scripts/db/connect.php
@@ -1,32 +1,20 @@
 <?php
-    class connect{
-        private $conx;
-        function __construct(){
-            try {
-                $this->conx = new PDO("mysql:host=172.16.48.204;port=3306;database=campusland;user=sputnik;password=Sp3tn1kC@");
-                echo "ok";
-            } catch (\Throwable $th) {
-                echo "Error";
-            }
-        }
-    }
-    
-    $obj = new Connect();
 
-
-    /* interface environments{
+    interface environments{
         public function __get($name);
     }
-    abstract class connect extends credentials implements environments{
+    class connect extends credentials implements environments{
         use getInstance;
         protected $conx;
         function __construct(private $driver = "mysql",  private $port = 3306){
             try {
                 $this->conx = new PDO($this->driver.":host=".$this->__get('host').";port=".$this->port.";dbname=".$this->__get('dbname').";user=".$this->user.";password=".$this->password);
                 $this->conx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "conexion exitosa";
             } catch (\PDOException $e) {
                 $this->conx = $e->getMessage();
             }
         }
-    } */
+    }
+    
 ?>
